98f63a9b1a6e35f07cb1595ad264a47d8cef8099
Calpurniax <sofia.gracia@gmail.com>
2024-02-12 18:04:03 +0100

    finish carousel with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css';
+import Carousel from './components/Carousel.component.jsx';
+import first from './assets/images/1.jpg';
+import second from './assets/images/2.jpg';
+import third from './assets/images/3.jpg';
 
+export default function App() {
+  let slides =[
+   first,
+   second,
+   third
+  ];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div className='w-[60%] m-auto'>
+        <Carousel slides={slides}/>
+      </div>      
     </>
-  )
+    )
 }
 
-export default App
+
---
+++ src/components/Carousel.component.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { GrCaretNext, GrCaretPrevious } from "react-icons/gr";
+
+
+export default function Carousel({ slides }) {
+
+    let [current, setCurrent] = useState(0)
+
+    let previousSlide =()=>{      
+        console.log(slides.length)  
+        if(current === 0) setCurrent(slides.length- 1);
+       
+        else setCurrent(current-1)
+    }
+    let nextSlide=()=>{        
+        if(current === slides.length - 1) setCurrent(0);
+        else setCurrent(current+1)
+    }
+    return (
+        <div className="overflow-hidden relative">
+            <div className="flex transition ease-out duration-40"
+            style={{ transform: `translateX(-${current*100}%)`}}>
+                {slides.map((s, i) => {
+                    return <img src={s} key={i}/>;
+                })}
+            </div>
+            <div className="absolute top-0 h-full w-full items-center justify-between flex text-black px-10">
+                <button onClick={previousSlide}>
+                    <GrCaretPrevious />
+                </button>
+                <button onClick={nextSlide}>
+                    <GrCaretNext />
+                </button>
+            </div>
+        </div>
+    );
+
+}
